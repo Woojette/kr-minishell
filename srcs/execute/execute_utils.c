a698df819c1d77d3708f9073245cbd3cd0ed6318
yookyeoc <yookyeoc@student.42.fr>
2026-01-31 18:11:21 +0100

    copy ys code

---
+++ srcs/execute/execute_utils.c
@@ -0,0 +1,70 @@
+
+#include "./mini_exec.h"
+
+void	free_path(char **path)
+{
+	int	i;
+
+	i = 0;
+	if (!path)
+		return ;
+	while (path[i])
+	{
+		free(path[i]);
+		i++;
+	}
+	free(path);
+}
+
+void	ft_close(int fd)
+{
+	int	result;
+
+	result = close(fd);
+	if (result == -1)
+	{
+		error_command("close");
+		exit (EXIT_FAILURE);
+	}
+}
+
+void	ft_dup2(int fd_from, int fd_to)
+{
+	int	result;
+
+	result = dup2(fd_from, fd_to);
+	if (result == -1)
+	{
+		error_command("dup2");
+		exit (EXIT_FAILURE);
+	}
+}
+
+void	update_exit(int process_number, pid_t cpid)
+{
+	int		i;
+	int		exit_tmp;
+	int		exit_status;
+	pid_t	pid_check;
+
+	i = -1;
+	while (++i < process_number)
+	{
+		pid_check = waitpid(-1, &exit_tmp, 0);
+		if (pid_check == cpid)
+			exit_status = exit_tmp;
+	}
+	if (exit_status == 2 || exit_status == 3)
+	{
+		if (exit_status == 3)
+			write(1, "Quit: 3\n", 8);
+		g_exit_status = 128 + exit_status;
+		return ;
+	}
+	if (WIFEXITED(exit_status))
+		g_exit_status = WEXITSTATUS(exit_status);
+	else if (WIFSIGNALED(exit_status))
+		g_exit_status = WTERMSIG(exit_status);
+	else
+		g_exit_status = WSTOPSIG(exit_status);
+}
---
+++ srcs/execute/heredoc.c
@@ -0,0 +1,78 @@
+
+#include "./mini_exec.h"
+
+void	child_heredoc(char *delimeter, char **env, int p_fd[2], char *new_file)
+{
+	int	fd;
+
+	signal_setting_heredocmode();
+	fd = open(new_file, O_CREAT | O_WRONLY, 0644);
+	if (fd == -1)
+		error_command("heredoc");
+	heredoc_write(fd, delimeter, env);
+	ft_close(p_fd[0]);
+	ft_close(p_fd[1]);
+	exit(0);
+}
+
+void	parent_heredoc(int p_fd[2], pid_t cpid)
+{
+	int	exit_status;
+
+	waitpid(cpid, &exit_status, 0);
+	if (exit_status == 2)
+	{
+		write(1, "\n", 1);
+		g_exit_status = 1;
+	}
+	ft_close(p_fd[0]);
+	ft_close(p_fd[1]);
+}
+
+int	run_heredoc(t_data *data, t_redirect *redirect)
+{
+	int			p_fd[2];
+	pid_t		cpid;
+	char		**env;
+	char		*new_file;
+	char		*delimeter;
+
+	env = data->my_env;
+	delimeter = redirect->filename;
+	if (pipe(p_fd) == -1)
+		error_command("pipe");
+	cpid = fork();
+	if (cpid == -1)
+		error_command("fork");
+	new_file = get_tmp_file();
+	if (cpid == 0)
+		child_heredoc(delimeter, env, p_fd, new_file);
+	else
+		parent_heredoc(p_fd, cpid);
+	free(redirect->filename);
+	redirect->filename = new_file;
+	return (0);
+}
+
+int	heredoc_center(t_data *data, t_pipeline *pipeline)
+{
+	t_redirect	*redirect;
+
+	g_exit_status = 0;
+	while (pipeline)
+	{
+		redirect = pipeline->cmd_block->redirect;
+		while (redirect)
+		{
+			if (redirect->type == 2)
+			{
+				run_heredoc(data, redirect);
+				if (g_exit_status == 1)
+					return (1);
+			}
+			redirect = redirect->next;
+		}
+		pipeline = pipeline->next;
+	}
+	return (0);
+}
