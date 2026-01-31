709b6f0c2bbb5c5aea5e9de8a3118555dfa372dc
yookyeoc <yookyeoc@student.42.fr>
2026-01-31 18:14:32 +0100

    child process if statement easy to read

--- srcs/execute/child_parent.c
+++ srcs/execute/child_parent.c
@@ -36,18 +36,32 @@ void	child_process(t_data *data, t_pipeline *pipeline, int *p_fd, int i)
 	signal_setting_commandmode();
 	if (data->process_number > 1)
 	{
-		if (i != 0)
+		if (i == 0)
 		{
-			ft_dup2(data->prev_fd, 0);
-			ft_close(data->prev_fd);
-		}
-		if (i != data->process_number - 1)
-		{
+			// 첫번째 커맨드 패밀리
 			ft_dup2(p_fd[1], 1);
 			data->prev_fd = p_fd[0];
+			ft_close(p_fd[0]);
+			ft_close(p_fd[1]);
 		}
-		ft_close(p_fd[0]);
-		ft_close(p_fd[1]);
+		else if (i != 0 && i != data->process_number - 1)
+		{
+			// 중간 커맨드 패밀리
+			ft_dup2(data->prev_fd, 0);
+			ft_close(data->prev_fd);
+			ft_dup2(p_fd[1], 1);
+			data->prev_fd = p_fd[0];
+			ft_close(p_fd[0]);
+			ft_close(p_fd[1]);
+		}
+		else
+		{
+			// 마지막 커맨드 패밀리
+			ft_dup2(data->prev_fd, 0);
+			ft_close(data->prev_fd);
+			ft_close(p_fd[0]);
+			ft_close(p_fd[1]);
+		}
 	}
 	if (redirection_center(pipeline->cmd_block->redirect))
 		g_exit_status = 1;
@@ -60,17 +74,25 @@ void	parent_process(t_data *data, int *p_fd, int i)
 {
 	if (data->process_number == 1)
 		return ;
+
 	if (i == 0)
-		data->prev_fd = p_fd[0];
-	else if (i == data->process_number - 1)
 	{
+		// 첫번째 커맨드 패밀리
+		data->prev_fd = p_fd[READ];
+		ft_close(p_fd[WRITE]);
+	}
+	else if (i != 0 && i != data->process_number - 1)
+	{
+		// 중간 커맨드 패밀리
 		ft_close(data->prev_fd);
-		ft_close(p_fd[READ]);
+		data->prev_fd = p_fd[READ];
+		ft_close(p_fd[WRITE]);
 	}
 	else
 	{
+		// 마지막 커맨드 패밀리
 		ft_close(data->prev_fd);
-		data->prev_fd = p_fd[READ];
+		ft_close(p_fd[READ]);
+		ft_close(p_fd[WRITE]);
 	}
-	ft_close((p_fd[WRITE]));
 }
